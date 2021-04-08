c5bbe5e4968e04ec0a5185a50dce9c05b49827c3
uniqss <uniqs@163.com>
2021-04-08 10:52:36 +0800

    eee

---
+++ tools/maps/pub.bat
@@ -0,0 +1,6 @@
+set DST=E:\games\w3__\Maps\maps
+
+copy /Y ".\3.9DAI0.875\真三国无双3.9D AI 0.875.w3x" %DST%\3.9DAI0.875\
+copy /Y ".\dota6.78cAI\DotA v6.78c AI 1.4e.w3x" %DST%\dota6.78cAI\
+
+usleep 5000
